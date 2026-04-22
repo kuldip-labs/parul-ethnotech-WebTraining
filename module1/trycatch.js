3eefb6479e3b62b33bd49fb1b944735b363ade4a
Kuldipkumar Prajapati <Kuldipgithub@outlook.com>
2026-04-22 13:30:43 +0530

    module1

--- module1/trycatch.js
+++ module1/trycatch.js
@@ -2,11 +2,13 @@
 try {
     // Code that might throw an error
     let result = JSON.parse('invalid json');
+
     console.log(result);
 } catch (error) {
     // Handle the error
+
     console.error('Error caught:', error.message);
     console.error('Error type:', error.name);
-}finally {
+} finally {
     console.log('This block will always execute, regardless of an error.');
 }
