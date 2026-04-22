2e8f4ffb2ec482ae40e806c9a67952145ee1825a
Kuldipkumar Prajapati <Kuldipgithub@outlook.com>
2026-04-22 14:50:09 +0530

    try catch external js

--- module1/trycatch.js
+++ module1/trycatch.js
@@ -9,6 +9,9 @@ try {
 
     console.error('Error caught:', error.message);
     console.error('Error type:', error.name);
+       
 } finally {
     console.log('This block will always execute, regardless of an error.');
 }
+
+
