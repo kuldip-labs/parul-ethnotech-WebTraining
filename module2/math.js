f36cfd5194bc3936618c9895df082d7b138394bc
Kuldipkumar Prajapati <Kuldipgithub@outlook.com>
2026-04-27 13:49:36 +0530

    math

---
+++ module2/math.js
@@ -0,0 +1,10 @@
+//math functions
+console.log(Math.PI); // Output: 3.141592653589793
+console.log(Math.abs(-5)); // Output: 5
+console.log(Math.ceil(4.2)); // Output: 5 Round up to the nearest whole number — 4.1 → 5
+console.log(Math.floor(4.8)); // Output: 4 Round down to the nearest whole number — 4.9 → 4
+console.log(Math.round(4.5)); // Output: 5 Round to the nearest integer — 4.5 → 5, 4.4 → 4
+console.log(Math.max(1, 2, 3, 4, 5)); // Output: 5
+console.log(Math.min(1, 2, 3, 4, 5)); // Output: 1
+console.log(Math.pow(2, 3)); // Output: 8
+console.log(Math.sqrt(16)); // Output: 4
