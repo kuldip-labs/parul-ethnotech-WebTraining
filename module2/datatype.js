37aba82557a332d491e7aecbfaa75f506391c203
Kuldipkumar Prajapati <Kuldipgithub@outlook.com>
2026-04-27 13:18:15 +0530

    str

---
+++ module2/datatype.js
@@ -0,0 +1,7 @@
+let arr = [1, 2, 3, 4, 5];
+console.log(typeof arr); // Output: "object"
+console.log(Array.isArray(arr)); // Output: true
+
+let obj = { name: "John", age: 30 };
+console.log(typeof obj); // Output: "object"
+console.log(Array.isArray(obj)); // Output: false
---
+++ module2/str.js
@@ -0,0 +1,15 @@
+//string methods
+let text = "Hello, World!";
+console.log(text.length); // Output: 13
+console.log(text.toUpperCase());
+// Output: "HELLO, WORLD!"
+console.log(text.toLowerCase());
+// Output: "hello, world!"
+console.log(text.includes("World")); // Output: true
+console.log(text.indexOf("o"));
+// Output: 4
+console.log(text.slice(0, 5)); // Output: "Hello"
+console.log(text.replace("World", "JavaScript"));
+// Output: "Hello, JavaScript!"
+
+
